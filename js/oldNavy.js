b4514b5ba7525c15a99010ce4fe394704e085a10
Emily Boerner <Emily.boerner@gmail.com>
2016-03-09 10:27:53 -0800

    add pages

--- js/oldNavy.js
+++ js/oldNavy.js
@@ -1,36 +1,38 @@
 
 
 
-$(".navbarToggle").click(function(){ 
-    $(".navigation").toggle();
-    $(".store-links, #postings, #map, #brand, .img-responsive").css({"opacity": "0.2"});
-    $(".navbar-text, .navbar-default, .navigation>li>a").css({"color": "black"});
-    $(".carousel-indicators, #input-box, #brand, img#search-2.search, #jumbo-banner, #social-and-banner-text").css({"opacity": "0.2"});
-});
+// $(".navbarToggle").click(function(){ 
+//     $(".navigation").toggle();
+//     $(".store-links, #postings, #map, #footer, #brand, .img-responsive").css({"opacity": "0.2"});
+//     $(".navbar-text, .navbar-default, .navigation>li>a").css({"color": "black"});
+//     $(".carousel-indicators, #input-box, #brand, img#search-2.search, #jumbo-banner, #footer, #social-and-banner-text").css({"opacity": "0.2"});
+// });
 
 
-$("a.nav-close").click(function(){
-    $(".carousel-indicators, #input-box, #brand, img#search-2.search, #jumbo-banner, #social-and-banner-text").css({"opacity": "1"});
-    $(".store-links, #postings, #map, #brand, .img-responsive").css({"opacity": "1"});
-    $(" .navigation").toggle();
-
-});
-
+// $("a.nav-close").click(function(){
+//     $(".carousel-indicators, #input-box, #brand, img#search-2.search, #jumbo-banner, #social-and-banner-text").css({"opacity": "1"});
+//     $(".store-links, #postings, #map, #brand, .img-responsive").css({"opacity": "1"});
+//     $(" .navigation").toggle();
+// });
 
 
 
-// $.getJSON('js/jobs.JSON', function (data){
+
+
+
+
+
+// $.getJSON('jobs.json', function (data){
 // 	$.each(data.stores, function(){
 // 		$("div#storeList").append('<ul><li class= "job-title" >' + this['job'] + 
 // 			'</li> <li class= "job-location">' + this['location'] +  
 // 			'</li> <li class= "job-description">' + this['description'] + 
 // 			'</li> <li class= "apply1">' + this['apply'] + '</li></ul><br/>');
 //   });
-
 // });
 
 
-// $.getJSON('js/jobs.JSON', function (data){
+// $.getJSON('jobs.JSON', function (data){
 // 	$.each(data.headquarters, function(){
 // 		$("div#stores").append('<ul><li class= "job-title" >' + this['job'] + 
 // 			'</li> <li class= "job-location">' + this['location'] +  
@@ -49,7 +51,4 @@ $("a.nav-close").click(function(){
 // 			'</li> <li class= "apply3">' + this['apply'] + '</li></ul><br/>');
 //   });
 // });
-
-
-
 
